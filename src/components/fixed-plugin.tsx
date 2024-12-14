d5732384fb6eca5312f9ead54a406d46824a0c1d
Elias Shemsu <echosierran@gmail.com>
2024-12-15 01:09:59 +0300

    bugfix

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -4,8 +4,11 @@ import { Button } from "@material-tailwind/react";
 
 export function FixedPlugin() {
   return (
-    <a href="https://www.material-tailwind.com" target="_blank">
+    <a>
       <Button
+        placeholder=""
+        onPointerEnterCapture={() => {}}
+        onPointerLeaveCapture={() => {}}
         color="white"
         size="sm"
         className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
